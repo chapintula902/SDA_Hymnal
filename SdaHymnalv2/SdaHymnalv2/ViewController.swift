b2ed010de6d9c1a79ec5f6d11aa10c9b1e653d69
chapintula902 <bgue969@lasierra.edu>
2020-10-09 22:29:02 -0400

    first commit

---
+++ SdaHymnalv2/SdaHymnalv2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SdaHymnalv2
+//
+//  Created by Bryan Guerra on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
